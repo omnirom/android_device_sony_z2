1c5735b8d0373647f55e7f50e479fe9cc1ba3fe4
Alexander Diewald <Diewi@diewald-net.com>
2017-09-14 14:31:08 +0200

    [DO NOT MERGE] Disable update assert & enable permissive.
    Change-Id: Ia10406005db5904e05734ea77220fb538605186d
    Signed-off-by: Alexander Diewald <Diewi@diewald-net.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,11 @@ include device/sony/shinano-common/BoardConfigCommonOmni.mk
 include $(DEVICE_PATH)/board/*.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := D6502,D6503,D6506,D6543,z2
+#TARGET_OTA_ASSERT_DEVICE := D6502,D6503,D6506,D6543,z2
+TARGET_OTA_ASSERT_DEVICE := none
+
+# Temporally set SELinux to permissive
+BOARD_USE_ENFORCING_SELINUX := false
 
 # inherit specific Header
 TARGET_SPECIFIC_HEADER_PATH += $(DEVICE_PATH)/include
