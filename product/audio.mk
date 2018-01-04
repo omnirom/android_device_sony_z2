497c6a3abe8809fc3ac3a8089f99a44162fa4e9d
Alexander Diewald <Diewi@diewald-net.com>
2018-01-04 18:33:12 +0100

    z2: audio: Adjust mixer paths for HAL update.
    The config files must be placed below "/vendor/etc" now.
    
    Change-Id: I665d2febc079ab4665fe934ec490658883299031
    Signed-off-by: Alexander Diewald <Diewi@diewald-net.com>

--- product/audio.mk
+++ product/audio.mk
@@ -1,4 +1,4 @@
 # Audio
 PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
-   $(LOCAL_PATH)/audio/mixer_paths_auxpcm.xml:system/etc/mixer_paths_auxpcm.xml
+   $(LOCAL_PATH)/audio/mixer_paths.xml:system/vendor/etc/mixer_paths.xml \
+   $(LOCAL_PATH)/audio/mixer_paths_auxpcm.xml:system/vendor/etc/mixer_paths_auxpcm.xml
