f24eff8f44fb770ef8e19889c9e4861facdda7aa
Alexander Diewald <Diewi@diewald-net.com>
2018-01-04 16:15:19 +0100

    z2: Commonize audio_policy in shinano-common.
    Change-Id: I1c4748975eb8d6b5adbe63e58d22417f7219734f
    Signed-off-by: Alexander Diewald <Diewi@diewald-net.com>

--- product/audio.mk
+++ product/audio.mk
@@ -1,5 +1,4 @@
 # Audio
 PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
    $(LOCAL_PATH)/audio/mixer_paths_auxpcm.xml:system/etc/mixer_paths_auxpcm.xml
