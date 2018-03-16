a8a2ea1288e7c33ac0d1d18f5de660a14c3dd465
Max Weffers <rcstar6696@gmail.com>
2018-03-16 10:54:30 +0100

    z2: Move device specific configs to [/vendor]/etc.
    * Use $(TARGET_COPY_OUT_VENDOR) to denote the vendor
      directory. This allows to use "real" treble setups
      where the vendor directory is a partition and those
      where a vendor directory in the system partition
      is used.
    
    Change-Id: I4b4d85b03aa6fc6a0ad15933742dba9827784a9a

--- product/radio.mk
+++ product/radio.mk
@@ -1,6 +1,6 @@
 # Radio
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
--- product/sensor.mk
+++ product/sensor.mk
@@ -1,7 +1,7 @@
 # Sensor
 PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/sensor/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf 
+   $(LOCAL_PATH)/sensor/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensor_def_qcomdev.conf 
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml 
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.proximity.xml 
